42bd32024432d8d6f72837572afc3a9e15de6cf5
Archit Sehgal <architsehgal19@gmail.com>
2024-03-16 23:27:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(cors())
 app.use("/", allRouter);
 app.use("/admin",adminRouter)
 // connect to mongodb
-mongoose.connect("mongodb+srv://architsehgal19:Architgr8@cluster0.1gxssef.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+mongoose.connect("mongodb cluster url", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
